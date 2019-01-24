14ae20b812bbee9b619fe724213dc4235aaf2be3
Mark Romero <markromero@Marks-MacBook-Pro.local>
2019-01-24 14:41:34 -0700

    completed drill 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      list: ['dog','cat','brid','shark']
+    }
+  }
+
+
   render() {
+    let animals = this.state.list.map( (element, index) => { 
+      return (
+        <h2 key={`${element}_${index}`} >
+         {element}
+        </h2>
+      );
+    });
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {animals}
       </div>
     );
   }
